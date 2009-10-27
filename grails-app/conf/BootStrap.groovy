4f0652693c1ad48fca99523d0a377dc5caab6210
Demian Neidetcher <demian0311@gmail.com>
2009-10-26 23:30:08 -0600

    add roles consistent with scrum

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,6 +4,9 @@ class BootStrap {
   def authenticateService
 
   def init = {servletContext ->
+
+    createRoles()
+
     switch (Environment.current) {
       case Environment.DEVELOPMENT:
       case Environment.PRODUCTION:
@@ -12,16 +15,31 @@ class BootStrap {
     }
   }
 
+  void createRoles() {
+      def scrumMaster = new Role(authority: "ROLE_SCRUM_MASTER", description: "scrum master")
+      scrumMaster.save()
+
+      def productOwner = new Role(authority: "ROLE_PRODUCT_OWNER", description: "product owner")
+      productOwner.save()
+
+      def pig = new Role(authority: "ROLE_TEAM_MEMBER", description: "someone committed to the project")
+      pig.save()
+
+      def chicken = new Role(authority: "ROLE_OBSERVER", description: "someone involved in the project")
+      chicken.save()
+  }
+
+
   void createAdminUserIfRequired() {
     if (!User.findByUsername("admin")) {
       log.debug "Creating admin user and role"
-      def role = new Role(authority: "ROLE_ADMINISTRATOR", description: "Super user")
+      def role = new Role(authority: "ROLE_ADMINISTRATOR", description: "administrator for the system")
       def user = new User(username: "admin", password: authenticateService.encodePassword("secret"), userRealName: "Administrator", email: "test@test.com", enabled: true)
       role.addToPeople(user)
       role.save()
-      def userRole = new Role(authority: "ROLE_USER", description: "Standard user")
-      userRole.addToPeople(user)
-      userRole.save()
+      //def userRole = new Role(authority: "ROLE_USER", description: "Standard user")
+      //userRole.addToPeople(user)
+      //userRole.save()
     } else {
       log.debug "Skipping creation of admin user"
     }
